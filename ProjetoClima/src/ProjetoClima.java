10b2fc33192863978aff68100574df3feb7e6d1c
Matheus Soares <dias.matheus@aluno.ufabc.edu.br>
2025-01-21 15:29:40 -0300

    Api ajustada e finalização do projeto

--- ProjetoClima/src/ProjetoClima.java
+++ ProjetoClima/src/ProjetoClima.java
@@ -1,3 +1,4 @@
+import java.nio.file.Files;
 import java.util.Scanner;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -6,19 +7,27 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
+import org.json.JSONObject;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import static java.net.http.HttpClient.newHttpClient;
 
 
-public class ProjetoClima{
-    public static void main(String[] args){
+public class ProjetoClima {
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite o nome da cidade: ");
-        String cidade= scanner.nextLine();
+        String cidade = scanner.nextLine();
 
-        try{
-            String dadosClimaticos= getdadosClimaticos(cidade);
-            if(dadosClimaticos.contains("\"code\":1006")){
+        try {
+            String dadosClimaticos = getdadosClimaticos(cidade);
+            if (dadosClimaticos.contains("\"code\":1006")) {
                 System.out.println("A localização inserida está incorreta!");
-            }else{
+            } else {
                 imprimirDadosClimaticos(dadosClimaticos);
             }
 
@@ -27,7 +36,60 @@ public class ProjetoClima{
         }
 
 
+    }
+
+    public static String getdadosClimaticos(String cidade) throws Exception {
+        String apiKey = Files.readString(Paths.get("API.txt")).trim();
+        System.out.println("api:" + apiKey);
+
+        String formatarNomeCidade = URLEncoder.encode(cidade, StandardCharsets.UTF_8);
+        String urlApi = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + formatarNomeCidade;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlApi))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("URL chamada: " + urlApi);
+        System.out.println("Resposta da API: " + response.body());
+        return response.body();
 
 
     }
+
+
+    public static void imprimirDadosClimaticos(String dados) {
+
+
+        JSONObject dadosJson = new JSONObject(dados);
+        JSONObject informacoesMeteorologicas = dadosJson.getJSONObject("current");
+
+        // dados de localização
+
+        String cidade = dadosJson.getJSONObject("location").getString("name");
+        String pais = dadosJson.getJSONObject("location").getString("country");
+
+        // dados climáticos:
+
+        String condicaoTempo = informacoesMeteorologicas.getJSONObject("condition").getString("text");
+        int umidade = informacoesMeteorologicas.getInt("humidity");
+        float velVento = informacoesMeteorologicas.getFloat("wind_kph");
+        float pressAtm = informacoesMeteorologicas.getFloat("pressure_mb");
+        float sensacaoTerm = informacoesMeteorologicas.getFloat("feelslike_c");
+        float temperaturaAtual = informacoesMeteorologicas.getFloat("temp_c");
+
+        String dataHora = informacoesMeteorologicas.getString("last_updated");
+        System.out.println("Informações:" + cidade + pais);
+        System.out.println("data e hora:" + dataHora);
+        System.out.println("Temperatura atual:" + temperaturaAtual + "°C");
+        System.out.println("Sensação térmica:" + sensacaoTerm + "°C");
+        System.out.println("Condição do tempo:" + condicaoTempo);
+        System.out.println("Umidade:" + umidade + "%");
+        System.out.println("Velocidade do vento:" + velVento + " km/h");
+        System.out.println("Pressão:" + pressAtm + "mb");
+
+    }
+
+
 }
